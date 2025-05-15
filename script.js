155231aa7597833f8b226be58affe58bed9a5e12
JeanHolanda <jeanpdholanda@gmail.com>
2025-05-15 10:36:55 -0300

    Primeiro Commit - Projeto Calculadora

---
+++ script.js
@@ -0,0 +1,36 @@
+const numberButtons = document.querySelectorAll("[id^='number']")
+const operatorButtons = document.querySelectorAll(".operator")
+const clearButton = document.querySelector("#clear")
+const display = document.querySelector("#display")
+const sum = document.querySelector("#sum")
+const sub = document.querySelector("#sub")
+const times = document.querySelector("#times")
+const over = document.querySelector("#over")
+const percent = document.querySelector("#percent")
+const plusOrMinus = document.querySelector("#plus-or-minus")
+const equal = document.querySelector("#equal")
+const squareRoot = document.querySelector("#square-root")
+const point = document.querySelector("#point")
+
+console.log(operatorButtons)
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        if(display.textContent === "0"){
+            display.textContent = e.target.value
+        }else{
+            display.textContent += e.target.value
+        }
+    
+    })
+})
+
+clearButton.addEventListener("click", () =>{
+    display.textContent = "0"
+})
+
+operatorButtons.forEach(button =>{
+    button.addEventListener ("click", (e) => {
+        display.textContent += e.target.value
+    })
+})
